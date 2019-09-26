39ad0bc28e309f0646f6e2c6110693af863231bd
Jesus R <arodriguez@arisoft.com.mx>
2019-09-26 09:47:04 -0600

    agrego consulta de previo

--- src/models/control_entrega.js
+++ src/models/control_entrega.js
@@ -1,20 +1,32 @@
 let dbCOBOL = require('../dbMacro');
 let consurModel = {};
 consurModel.getPrevioCompra = (codigo, callback) => {
-    //console.log(codigo);
     if (dbCOBOL) {
         dbCOBOL.query(`SELECT 
-        CREN_OPE,
-        CREN_FOL,
-        CREN_FCH,
-        CREN_ART,
-        CREN_CANT,
-        CREN_SURT,
-        CREN_STAT
+        CREN_ART as articulo,
+        CREN_CANT as cantidad,
+        CREN_SURT as surtido
         FROM PUBLIC.COMREN
-        WHERE PUBLIC.COMREN.CREN_OPE=1 
-        AND PUBLIC.COMREN.CREN_STAT='A'
+        WHERE PUBLIC.COMREN.CREN_OPE=1
         AND PUBLIC.COMREN.CREN_FOL='` + codigo + `'
+    
+    `, function(err, rows) {
+            if (err) {
+                //throw err;
+                callback(err, null);
+            } else {
+                callback(null, rows);
+            }
+        });
+    }
+};
+consurModel.getEstatus = (codigo, callback) => {
+    if (dbCOBOL) {
+        dbCOBOL.query(`SELECT 
+        CDOC_STAT as ESTATUS
+        FROM PUBLIC.COMDOC
+        WHERE PUBLIC.COMDOC.CDOC_OPE=1
+        AND PUBLIC.COMDOC.CDOC_FOL='` + codigo + `'
     
     `, function(err, rows) {
             if (err) {
--- src/routes/control_entrega.js
+++ src/routes/control_entrega.js
@@ -4,20 +4,56 @@ module.exports = function(app) {
 
     app.get('/consulta_previo/:codigo', (req, res) => {
         let codigo = req.params.codigo;
-        controlEntrega.getPrevioCompra(codigo, (err, data) => {
+        let estatus = "";
+        controlEntrega.getEstatus(codigo, (err, data) => {
             if (err) {
                 res.status(500).send({
                     success: false,
                     message: 'Error al consultar previo:' + err
                 });
             } else {
+                if (data.length == 0) {
+                    res.json({
+                        success: true,
+                        message: 'no existe folio'
+                    });
 
-                res.json({
-                    success: true,
-                    previo: data
-                });
+                } else {
+                    estatus = data[0].ESTATUS;
+                    if (estatus == "S") {
+                        res.json({
+                            success: true,
+                            message: 'surtidas'
+                        });
+                    } else {
+                        if (estatus == "A") {
+                            controlEntrega.getPrevioCompra(codigo, (err, data) => {
+                                if (err) {
+                                    res.status(500).send({
+                                        success: false,
+                                        message: 'Error al consultar previo:' + err
+                                    });
+                                } else {
+
+                                    res.json({
+                                        success: true,
+                                        previo: data
+                                    });
+                                }
+                            });
+
+                        }
+                    }
+
+                }
+
             }
 
+
+
+
         });
+
+
     });
 }
