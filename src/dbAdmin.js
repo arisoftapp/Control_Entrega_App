cc7081491685bc0cc8c53d591cf51aa774ab3583
Jesus R <arodriguez@arisoft.com.mx>
2019-09-26 10:57:31 -0600

    contraseña bd

--- src/dbAdmin.js
+++ src/dbAdmin.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'arisoft.2019',
     database: 'controlre_db'
 });
 
